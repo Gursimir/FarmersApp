9e2c4e5a5bca9050efa8daff7e655b43a3ff8ad7
Gursimir <kgursimran648@gmail.com>
2020-09-23 00:26:57 +0530

    commit

--- app/src/main/java/com/example/splashscreen/MainActivity.java
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
+
+import com.example.splashscreen.ui.login.Login;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler h;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent in = new Intent(MainActivity.this, Login.class);
+                startActivity(in);
+            }
+        },1000);
     }
+
 }
