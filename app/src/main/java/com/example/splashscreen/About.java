7f82f1beac241f2f6b43e1e3de5b0cede1f56c8d
Gursimir <kgursimran648@gmail.com>
2020-09-24 21:50:55 +0530

    commit

---
+++ app/src/main/java/com/example/splashscreen/About.java
@@ -0,0 +1,21 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
--- app/src/main/java/com/example/splashscreen/Home.java
+++ app/src/main/java/com/example/splashscreen/Home.java
@@ -1,11 +1,15 @@
 package com.example.splashscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class Home extends AppCompatActivity {
 
@@ -21,5 +25,34 @@ public class Home extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.home :
+                Intent intent = new Intent(Home.this,Home.class);
+                startActivity(intent);
+                break;
+
+            case R.id.aboutus :
+                Intent intent1 = new Intent(Home.this,About.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.contactus :
+                Intent intent2 = new Intent(Home.this,Contact.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.logout :
+                Intent intent3 = new Intent(Home.this,Login.class);
+                startActivity(intent3);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
